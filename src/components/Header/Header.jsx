db2ababbc1b466a7f8c9929f4264a510cbf19efd
khanzafer448 <khanzafer448@gmail.com>
2022-06-25 17:13:13 +0530

    post page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
 
+import "./header.css";
+
 const Header = () => {
   return (
     <div className="header">
@@ -8,21 +10,14 @@ const Header = () => {
           <Navbar.Brand href="#home">Social Media</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">About</Nav.Link>
               <Nav.Link href="#link">Blog</Nav.Link>
               <Nav.Link href="#link">Services</Nav.Link>
+
+              <Button variant="outline-success">Create Post</Button>
             </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
